8cf7f135c626d1433bbd776c55192c3ed00ceca6
Urban Hafner <contact@urbanhafner.com>
2015-02-28 10:10:18 +0100

    Fix test that failed due to stdlib changing

--- src/board/chain/test.rs
+++ src/board/chain/test.rs
@@ -11,6 +11,6 @@ fn show_returns_a_legible_string_for_the_chain() {
     c.add_coord(Coord::new(7,8));
     c.add_coord(Coord::new(7,9));
 
-    let expected = String::from_str("1  | Black, libs: HashSet {\"(1,1)\"}, stones: [\"(7,7)\", \"(7,8)\", \"(7,9)\"]");
+    let expected = String::from_str("1  | Black, libs: {\"(1,1)\"}, stones: [\"(7,7)\", \"(7,8)\", \"(7,9)\"]");
     assert_eq!(c.show(), expected);
 }
