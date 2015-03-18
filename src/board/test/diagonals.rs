6d2ccc6b05719e510bd137b09bf3c0f5447a3735
Urban Hafner <contact@urbanhafner.com>
2015-03-18 08:54:38 +0100

    Rename orthogonals to diagonals
    I don't know what I was thinking when I named those functions and fields
    orthogonals when they're clearly the diagonal neighbours.

--- src/board/test/diagonals.rs
+++ src/board/test/diagonals.rs
@@ -28,78 +28,78 @@ use ruleset::Minimal;
 #[test]
 fn center_has_4_orthogonal_neighbours() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(2, 2));
-    assert_eq!(4, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(1, 1)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(1, 3)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(3, 1)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(3, 3)).is_some());
+    let diagonals = b.diagonals(Coord::new(2, 2));
+    assert_eq!(4, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(1, 1)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(1, 3)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(3, 1)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(3, 3)).is_some());
 }
 
 #[test]
 fn sw_has_1_orthogonal_neighbour() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(1, 1));
-    assert_eq!(1, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
+    let diagonals = b.diagonals(Coord::new(1, 1));
+    assert_eq!(1, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
 }
 
 #[test]
 fn s_has_2_orthogonal_neighbours() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(2, 1));
-    assert_eq!(2, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(1, 2)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(3, 2)).is_some());
+    let diagonals = b.diagonals(Coord::new(2, 1));
+    assert_eq!(2, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(1, 2)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(3, 2)).is_some());
 }
 
 #[test]
 fn se_has_1_orthogonal_neighbour() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(3, 1));
-    assert_eq!(1, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
+    let diagonals = b.diagonals(Coord::new(3, 1));
+    assert_eq!(1, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
 }
 
 #[test]
 fn w_has_2_orthogonal_neighbours() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(1, 2));
-    assert_eq!(2, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 1)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 3)).is_some());
+    let diagonals = b.diagonals(Coord::new(1, 2));
+    assert_eq!(2, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 1)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 3)).is_some());
 }
 
 #[test]
 fn e_has_2_orthogonal_neighbours() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(3, 2));
-    assert_eq!(2, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 1)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 3)).is_some());
+    let diagonals = b.diagonals(Coord::new(3, 2));
+    assert_eq!(2, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 1)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 3)).is_some());
 }
 
 #[test]
 fn nw_has_1_orthogonal_neighbour() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(1, 3));
-    assert_eq!(1, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
+    let diagonals = b.diagonals(Coord::new(1, 3));
+    assert_eq!(1, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
 }
 
 #[test]
 fn n_has_2_orthogonal_neighbours() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(2, 3));
-    assert_eq!(2, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(1, 2)).is_some());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(3, 2)).is_some());
+    let diagonals = b.diagonals(Coord::new(2, 3));
+    assert_eq!(2, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(1, 2)).is_some());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(3, 2)).is_some());
 }
 
 #[test]
 fn ne_has_1_orthogonal_neighbour() {
     let b = Board::new(3, 6.5, Minimal);
-    let orthogonals = b.orthogonals(Coord::new(3, 3));
-    assert_eq!(1, orthogonals.len());
-    assert!(orthogonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
+    let diagonals = b.diagonals(Coord::new(3, 3));
+    assert_eq!(1, diagonals.len());
+    assert!(diagonals.iter().find(|&c| *c == Coord::new(2, 2)).is_some());
 }
