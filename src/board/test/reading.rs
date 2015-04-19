b8261211d914a0bb93bd12a62120e7e724516eb2
Igor Polyakov <iopguy@gmail.com>
2015-04-18 17:01:22 -0700

    with these files too

---
+++ src/board/test/reading.rs
@@ -0,0 +1,80 @@
+/************************************************************************
+ *                                                                      *
+ * Copyright 2015 Igor Polyakov                                         *
+ *                                                                      *
+ * This file is part of Iomrascálaí.                                    *
+ *                                                                      *
+ * Iomrascálaí is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * Iomrascálaí is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with Iomrascálaí.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                      *
+ ************************************************************************/
+ 
+#![cfg(test)]
+use std::path::Path;
+use board::{Black, Coord, Play};
+use sgf::Parser;
+
+#[test]
+fn bottom_left_escapes_ladder() {
+    let parser = Parser::from_path(Path::new("fixtures/sgf/ladders.sgf")).unwrap();
+    let game   = parser.game().unwrap();
+    let board  = game.board();
+    let coord = Coord { col: 4, row: 4 };
+    let chain = board.get_chain(coord).unwrap();
+    
+    assert_eq!(1, board.save_group(&chain).len());
+}
+
+#[test]
+fn bottom_left_is_ladder() {
+    let parser = Parser::from_path(Path::new("fixtures/sgf/ladders.sgf")).unwrap();
+    let game   = parser.game().unwrap();
+    let board  = game.board();
+    let coord = Coord { col: 4, row: 4 };
+    let chain = board.get_chain(coord).unwrap();
+
+    assert_eq!(Play(Black, 4, 5), board.clone().capture_ladder(&chain).unwrap());
+}
+
+#[test]
+fn top_left_is_not_ladder() {
+    let parser = Parser::from_path(Path::new("fixtures/sgf/ladders.sgf")).unwrap();
+    let game   = parser.game().unwrap();
+    let board  = game.board();
+    let coord = Coord { col: 4, row: 15 };
+    let chain = board.get_chain(coord).unwrap();
+
+    assert_eq!(None, board.clone().capture_ladder(&chain));
+}
+
+#[test]
+fn top_right_is_not_ladder() {
+    let parser = Parser::from_path(Path::new("fixtures/sgf/ladders.sgf")).unwrap();
+    let game   = parser.game().unwrap();
+    let board  = game.board();
+    let coord = Coord { col: 15, row: 15 };
+    let chain = board.get_chain(coord).unwrap();
+
+    assert_eq!(None, board.clone().capture_ladder(&chain));
+}
+
+#[test]
+fn bottom_right_is_ladder() {
+    let parser = Parser::from_path(Path::new("fixtures/sgf/ladders.sgf")).unwrap();
+    let game   = parser.game().unwrap();
+    let board  = game.board();
+    let coord = Coord { col: 5, row: 16 };
+    let chain = board.get_chain(coord).unwrap();
+
+    assert_eq!(None, board.clone().capture_ladder(&chain));
+}
