98bc9845d569cd3b33a270f75e1a992d3ae7e601
Urban Hafner <contact@urbanhafner.com>
2015-09-03 10:46:07 +0200

    3x3 patterns from michi

--- src/patterns/mod.rs
+++ src/patterns/mod.rs
@@ -32,6 +32,69 @@ impl Matcher {
         Matcher
     }
 
+    // Patterns liften from michi.py
+    // * X, O are colors
+    // * . is an empty intersection
+    // * x, o are the opposites of X,O (i.e. other color or empty)
+    // * SPACE is off board
+    // * ? is any color, empty intersection, or off board
+    fn patterns() -> Vec<Vec<&'static str>> {
+        vec!(
+            // hane pattern - enclosing hane
+            vec!("XOX",
+                 "...",
+                 "???"),
+            // hane pattern - non-cutting hane
+            vec!("XO.",
+                 "...",
+                 "?.?"),
+            // hane pattern - magari
+            vec!("XO?",
+                 "X..",
+                 "x.?"),
+            // generic pattern - katatsuke or diagonal attachment; similar to magari
+            vec!(".O.",
+                 "X..",
+                 "..."),
+            // cut1 pattern (kiri] - unprotected cut
+            vec!("XO?",
+                 "O.o",
+                 "?o?"),
+            // cut1 pattern (kiri] - peeped cut
+            vec!("XO?",
+                 "O.X",
+                 "???"),
+            // cut2 pattern (de]
+            vec!("?X?",
+                 "O.O",
+                 "ooo"),
+            // cut keima
+            vec!("OX?",
+                 "o.O",
+                 "???"),
+            // side pattern - chase
+            vec!("X.?",
+                 "O.?",
+                 "  ?"),
+            // side pattern - block side cut
+            vec!("OX?",
+                 "X.O",
+                 "   "),
+            // side pattern - block side connection
+            vec!("?X?",
+                 "x.O",
+                 "   "),
+            // side pattern - sagari
+            vec!("?XO",
+                 "x.x",
+                 "   "),
+            // side pattern - cut
+            vec!("?OX",
+                 "X.O",
+                 "   "),
+            )
+    }
+
     pub fn pattern_count(&self, _: &Board, _: &Move) -> usize {
         0
     }
