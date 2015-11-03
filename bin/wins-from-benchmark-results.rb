7b5db9e6499efee57d3a0ff4fe1bba1efd0e3e83
Urban Hafner <contact@urbanhafner.com>
2015-11-03 10:50:07 +0100

    Calculate 95% and 99% percent confidence intervals from test data
    It turns out that the error that gogui-twogtp reports is just the
    standard deviation. This of course means that the result will fall
    within that range 68% of the time only!
    
    I've now enhanced my calculation script to report both the 2sigma (95%
    confidence) and 3sigma (99% confidence) intervals.
    
    It appears that just running 200 games isn't really good
    enough (anymore).

--- bin/wins-from-benchmark-results.rb
+++ bin/wins-from-benchmark-results.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def data(contents)
   contents.each_line.find_all {|l| l !~ /^#/ }.map do |l|
     l.split(/\s+/)[3]
@@ -8,8 +9,11 @@ def wins(file)
   contents = File.read(file)
   white = data(contents).find_all {|l| l =~ /W\+/ }.count
   black = data(contents).find_all {|l| l =~ /B\+/ }.count
-  percentage = white.to_f/(white+black)
-  "#{(percentage*100).round(2)}% wins (#{white} games of #{white+black})"
+  n = white + black
+  mean = (white.to_f/(white+black))*100
+  variance = (white * (100-mean) + black * mean) / (n-1)
+  sigma = Math.sqrt(variance) / 2
+  "#{mean.round(2)}% wins (#{white} games of #{n}, ± #{(2*sigma).round(2)} at 95%, ± #{(3*sigma).round(2)} at 99%)"
 end
 
 Dir["*.dat"].each do |fn|
