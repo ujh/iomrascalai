c53e5cf78dd865da1b07dc06ef927f141789febe
Urban Hafner <contact@urbanhafner.com>
2015-11-03 21:03:07 +0100

    Use Binomial proportion confidence interval for the error calculation

--- bin/wins-from-benchmark-results.rb
+++ bin/wins-from-benchmark-results.rb
@@ -10,10 +10,17 @@ def wins(file)
   white = data(contents).find_all {|l| l =~ /W\+/ }.count
   black = data(contents).find_all {|l| l =~ /B\+/ }.count
   n = white + black
-  mean = (white.to_f/(white+black))*100
-  variance = (white * (100-mean) + black * mean) / (n-1)
-  sigma = Math.sqrt(variance) / 2
-  "#{mean.round(2)}% wins (#{white} games of #{n}, ± #{(2*sigma).round(2)} at 95%, ± #{(3*sigma).round(2)} at 99%)"
+  p = white.to_f/n
+  "#{(p*100).round(2)}% wins (#{white} games of #{n}, ± #{error(p: p, n: n, confidence: 0.95).round(2)} at 95%, ± #{error(p: p, n: n, confidence: 0.99).round(2)} at 99%)"
+end
+
+def z(confidence:)
+  alpha = 1 - confidence
+  (1 - 0.5*alpha)*2
+end
+
+def error(p:, n:, confidence:)
+  (z(confidence: confidence) * Math.sqrt((1.0/n)*p*(1-p)))*100
 end
 
 Dir["*.dat"].each do |fn|
