041662d0cc8a1ea6afaa78ec7beb747433199a5b
Urban Hafner <contact@urbanhafner.com>
2015-11-12 08:34:17 +0100

    CLOP experiment configuration

--- misc/clop/runner.rb
+++ misc/clop/runner.rb
@@ -1,6 +1,6 @@
-#!/usr/bin/env ruby
-
 require 'fileutils'
+
+$stderr.puts ARGV.inspect
 
 processor = ARGV[0] # ignored
 SEED = ARGV[1]
@@ -11,39 +11,38 @@ def run(command)
 end
 
 parameters = {
-  play_out_aftermath: 0.0,
-  threads: 8.0,
+  play_out_aftermath: 0,
+  threads: 8,
   playout: {
-    atari_check: 1.0,
-    ladder_check: 1.0,
-    last_moves_for_heuristics: 2.0,
-    no_self_atari_cutoff: 7.0,
+    atari_check: 1, # boolean
+    ladder_check: 1, # boolean
+    last_moves_for_heuristics: 2,
+    no_self_atari_cutoff: 7,
     pattern_probability: 0.9,
-    play_in_middle_of_eye: 1.0,
-    use_patterns: 1.0,
+    play_in_middle_of_eye: 1, # boolean
+    use_patterns: 1, # boolean
   },
   priors: {
     best_move_factor: 1.0,
-    capture_many: 30.0,
-    capture_one: 15.0,
-    empty: 20.0,
-    neutral_plays: 10.0,
-    neutral_wins: 5.0,
-    patterns: 10.0,
-    self_atari: 10.0,
-    use_empty: 1.0,
-    use_patterns: 0.0,
+    capture_many: 30,
+    capture_one: 15,
+    empty: 20,
+    neutral_plays: 10,
+    neutral_wins: 5,
+    patterns: 10,
+    self_atari: 10,
+    use_empty: 1, # boolean
+    use_patterns: 0, # boolean
   },
   timer: {
     c: 0.5,
   },
   tree: {
     end_of_game_cutoff: 0.08,
-    expand_after: 1.0,
+    expand_after: 1,
     fastplay20_thres: 0.8,
     fastplay5_thres: 0.95,
     rave_equiv: 20.0,
-    reuse_subtree: 1.0
   }
 }
 
@@ -53,26 +52,20 @@ ARGV[2..-1].each_slice(2) do |name, value|
   parameters[level1][level2] = value
 end
 
-def bool(float)
-  (float > 0.5).inspect
-end
-
-def int(float)
-  float.round
+def bool(number)
+  number.round.zero?.inspect
 end
 
 config = <<EOS
-log = true
 play_out_aftermath = #{bool(parameters[:play_out_aftermath])}
-ruleset = "chinese"
-threads = #{int(parameters[:threads])}
+threads = #{parameters[:threads]}
 
 [playout]
 
 atari_check = #{bool(parameters[:playout][:atari_check])}
 ladder_check = #{bool(parameters[:playout][:ladder_check])}
-last_moves_for_heuristics = #{int(parameters[:playout][:last_moves_for_heuristics])}
-no_self_atari_cutoff = #{int(parameters[:playout][:no_self_atari_cutoff])}
+last_moves_for_heuristics = #{parameters[:playout][:last_moves_for_heuristics]}
+no_self_atari_cutoff = #{parameters[:playout][:no_self_atari_cutoff]}
 pattern_probability = #{parameters[:playout][:pattern_probability]}
 play_in_middle_of_eye = #{bool(parameters[:playout][:play_in_middle_of_eye])}
 use_patterns = #{bool(parameters[:playout][:use_patterns])}
@@ -80,13 +73,13 @@ use_patterns = #{bool(parameters[:playout][:use_patterns])}
 [priors]
 
 best_move_factor = #{parameters[:priors][:best_move_factor]}
-capture_many = #{int(parameters[:priors][:capture_many])}
-capture_one = #{int(parameters[:priors][:capture_one])}
-empty = #{int(parameters[:priors][:empty])}
-neutral_plays = #{int(parameters[:priors][:neutral_plays])}
-neutral_wins = #{int(parameters[:priors][:neutral_wins])}
-patterns = #{int(parameters[:priors][:patterns])}
-self_atari = #{int(parameters[:priors][:self_atari])}
+capture_many = #{parameters[:priors][:capture_many]}
+capture_one = #{parameters[:priors][:capture_one]}
+empty = #{parameters[:priors][:empty]}
+neutral_plays = #{parameters[:priors][:neutral_plays]}
+neutral_wins = #{parameters[:priors][:neutral_wins]}
+patterns = #{parameters[:priors][:patterns]}
+self_atari = #{parameters[:priors][:self_atari]}
 use_empty = #{bool(parameters[:priors][:use_empty])}
 use_patterns = #{bool(parameters[:priors][:use_patterns])}
 
@@ -97,19 +90,27 @@ c = #{parameters[:timer][:c]}
 [tree]
 
 end_of_game_cutoff = #{parameters[:tree][:end_of_game_cutoff]}
-expand_after = #{int(parameters[:tree][:expand_after])}
+expand_after = #{parameters[:tree][:expand_after]}
 fastplay20_thres = #{parameters[:tree][:fastplay20_thres]}
 fastplay5_thres = #{parameters[:tree][:fastplay5_thres]}
 rave_equiv = #{parameters[:tree][:rave_equiv]}
-reuse_subtree = #{bool(parameters[:tree][:reuse_subtree])}
 EOS
 
 GNUGO = "gnugo --mode gtp --level 0 --chinese-rules --positional-superko --capture-all-dead --score aftermath --play-out-aftermath"
 REFEREE = GNUGO
 IOMRASCALAI = "cargo run --release -- -c config.toml"
 
-SIZE=9
-TIME="1m"
+case SIZE
+when 9
+  TIME = "5m"
+when 13
+  TIME = "10m"
+when 19
+  TIME = "20m"
+else
+  puts "Size #{SIZE} not supported!"
+  exit 1
+end
 
 if rand(2).zero?
   WHITE = GNUGO
