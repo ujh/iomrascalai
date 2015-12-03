5b1e68ed611580c5d4f0b0500b755507cc5ff1b4
Urban Hafner <contact@urbanhafner.com>
2015-12-03 22:07:03 +0100

    Split up test cases

--- src/score/test.rs
+++ src/score/test.rs
@@ -37,34 +37,73 @@ pub use ruleset::Minimal;
 
 describe! score {
 
-    it "counting the simple case" {
-        let b = load_board("score/simple");
-        let score = b.score();
-        assert_that(score.black_stones, is(equal_to(8)));
-        assert_that(score.white_stones, is(equal_to(8)));
-        assert_that(score.color(), is(equal_to(White)));
-        assert_that(score.score(), is(equal_to(6.5)));
-        assert_that(format!("{}", score), is(equal_to("W+6.5".to_string())));
+    describe! simple {
+
+        before_each {
+            let score = load_board("score/simple").score();
+        }
+
+        it "counting" {
+            assert_that(score.black_stones, is(equal_to(8)));
+            assert_that(score.white_stones, is(equal_to(8)));
+        }
+
+        it "score" {
+            assert_that(score.color(), is(equal_to(White)));
+            assert_that(score.score(), is(equal_to(6.5)));
+            assert_that(format!("{}", score), is(equal_to("W+6.5".to_string())));
+        }
+
+        it "ownership" {
+            // TODO
+        }
+
     }
 
-    it "counting disjoint territory" {
-        let b = load_board("score/disjoint");
-        let score = b.score();
-        assert_that(score.black_stones, is(equal_to(9)));
-        assert_that(score.white_stones, is(equal_to(16)));
-        assert_that(score.color(), is(equal_to(White)));
-        assert_that(score.score(), is(equal_to(13.5)));
-        assert_that(format!("{}", score), is(equal_to("W+13.5".to_string())));
+    describe! disjoint_territory {
+
+        before_each {
+            let score = load_board("score/disjoint").score();
+        }
+
+        it "counting" {
+            assert_that(score.black_stones, is(equal_to(9)));
+            assert_that(score.white_stones, is(equal_to(16)));
+        }
+
+        it "score" {
+            assert_that(score.color(), is(equal_to(White)));
+            assert_that(score.score(), is(equal_to(13.5)));
+            assert_that(format!("{}", score), is(equal_to("W+13.5".to_string())));
+        }
+
+        it "ownership" {
+            // TODO
+        }
+
     }
 
-    it "counting with neutral points" {
-        let b = load_board("score/dame");
-        let score = b.score();
-        assert_that(score.black_stones, is(equal_to(4)));
-        assert_that(score.white_stones, is(equal_to(20)));
-        assert_that(score.color(), is(equal_to(White)));
-        assert_that(score.score(), is(equal_to(22.5)));
-        assert_that(format!("{}", score), is(equal_to("W+22.5".to_string())));
+    describe! dame {
+
+        before_each {
+            let score = load_board("score/dame").score();
+        }
+
+        it "counting" {
+            assert_that(score.black_stones, is(equal_to(4)));
+            assert_that(score.white_stones, is(equal_to(20)));
+        }
+
+        it "score" {
+            assert_that(score.color(), is(equal_to(White)));
+            assert_that(score.score(), is(equal_to(22.5)));
+            assert_that(format!("{}", score), is(equal_to("W+22.5".to_string())));
+        }
+
+        it "ownership" {
+            // TODO
+        }
+
     }
 
 }
